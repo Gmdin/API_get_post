b67ddb18c098f6bbaf731d83efb1b1d9c80f6193
unknown <gmdin716@gmail.com>
2023-01-09 23:42:02 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { useState } from 'react';
+import './App.css';
+import DemoOutput from './components/DemoOutput';
 function App() {
+  const [data,setData]=useState(false);
+
+  const hideData=()=>{
+    setData((prevData)=>!prevData);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <h1>Hi There!</h1>
+        {/* {data && <p>I am a paragraph</p>} */}
+        <DemoOutput show={data}/>
+          <button onClick={hideData}>Button</button>
     </div>
   );
 }
