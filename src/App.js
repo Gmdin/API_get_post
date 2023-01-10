85f9c5dac5aa63e4b39364172a71f89d4d3e6d15
unknown <gmdin716@gmail.com>
2023-01-10 22:32:17 +0500

    fetch movies from estApi And get movies detail through input

--- src/App.js
+++ src/App.js
@@ -1,20 +1,72 @@
+import React, { useState, useEffect, useCallback } from 'react';
 
-import { useState } from 'react';
+import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
-import DemoOutput from './components/DemoOutput';
-function App() {
-  const [data,setData]=useState(false);
 
-  const hideData=()=>{
-    setData((prevData)=>!prevData);
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch('https://swapi.dev/api/films/');
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  function addMovieHandler(movie) {
+    console.log(movie);
   }
+
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
   return (
-    <div className="App">
-          <h1>Hi There!</h1>
-        {/* {data && <p>I am a paragraph</p>} */}
-        <DemoOutput show={data}/>
-          <button onClick={hideData}>Button</button>
-    </div>
+    <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>{content}</section>
+    </React.Fragment>
   );
 }
 
